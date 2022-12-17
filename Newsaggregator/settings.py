0c9bce891f5fbf12c7da43984ae473332ea72595
Murtazokh <murtazoxon12@gmail.com>
2022-12-17 21:41:52 +0900

    a

--- Newsaggregator/settings.py
+++ Newsaggregator/settings.py
@@ -79,9 +79,16 @@ WSGI_APPLICATION = 'Newsaggregator.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        # 'USER': 'postgres',
+        # 'PASSWORD':'1qaz2wsx',
+        # 'HOST':'localhost',
+        # 'PORT':'5432'
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
